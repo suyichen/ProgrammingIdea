60b0eaf0cb98717657ac128b887ca5c6c774fd61
suyichen <805788048@qq.com>
2019-04-10 00:10:33 +0800

    add construction method

---
+++ app/src/main/java/method/construction/Son.java
@@ -0,0 +1,20 @@
+package method.construction;
+
+/**
+ * @author suyichen
+ */
+public class Son {
+
+    private String name;
+    private int age;
+
+    Son(){
+        this.name = "Nick";
+        this.age = 30;
+    }
+
+    Son(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+}
